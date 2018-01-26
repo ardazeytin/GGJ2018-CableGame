471c4e5de65d7bf810483860cc4bcf37f7e2d433
Arda Zeytin <ardazeytin@outlook.com>
2018-01-27 02:01:55 +0300

    first commit
    before raycast

---
+++ Assets/PlayerController.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+    public float rotSpeed;
+    public float moveSpeed;
+
+	// Use this for initialization
+	void Start ()
+    {
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        transform.Rotate(Vector3.up * rotSpeed * Time.deltaTime);
+        transform.Translate(Vector3.up * moveSpeed * Time.deltaTime);
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            int random = Random.Range(1, 4);
+
+            if (random==1)
+            {
+                this.transform.position = new Vector3(-2, 0, 0);
+            }
+            else if (random==2)
+            {
+                this.transform.position = new Vector3(2, 0, 0);
+            }
+            else if (random==3)
+            {
+                this.transform.position = new Vector3(0, 2, 0);
+            }
+        }
+	}
+
+
+}
---
+++ Assets/Spawner.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawner : MonoBehaviour {
+
+    //cable spawn center points
+    public Vector3 centerNodePoint;
+    public Vector3 nodePoint1;
+    public Vector3 nodePoint2;
+    public Vector3 nodePoint3;
+
+    //cable prefabs
+    public Transform singleCable1;
+
+
+    void Start ()
+    {
+        centerNodePoint = new Vector3(0, 0, 0);
+        nodePoint1 = new Vector3(-1, 1, 0); //top left
+        nodePoint2 = new Vector3(1, 1, 0); //top right
+        nodePoint3 = new Vector3(0, -1, 0); //bottom center
+    }
+	
+	// Update is called once per frame
+	void Update ()
+    {
+		
+	}
+
+    private void OnTriggerEnter(Collider other)
+    {
+        SpawnCable();
+    }
+
+    void SpawnCable()
+    {
+        //vector3 position will change
+        Instantiate(singleCable1, new Vector3(0, 0, transform.parent.position.z + 154f), singleCable1.rotation);
+    }
+}
