6a279b73b68198f382b59e2dbfa55900a221fa29
Arda Zeytin <ardazeytin@outlook.com>
2018-01-27 03:13:55 +0300

    nodejump mekaniği

--- Assets/PlayerController.cs
+++ Assets/PlayerController.cs
@@ -21,25 +21,37 @@ public class PlayerController : MonoBehaviour {
 
         if (Input.GetKeyDown(KeyCode.Space))
         {
-            int random = Random.Range(1, 4);
+            //int random = Random.Range(1, 4);
 
-            if (random==1)
-            {
-                this.transform.position = new Vector3(-2, 0, 0);
-            }
-            else if (random==2)
-            {
-                this.transform.position = new Vector3(2, 0, 0);
-            }
-            else if (random==3)
-            {
-                this.transform.position = new Vector3(0, 2, 0);
-            }
+            //if (random==1)
+            //{
+            //    this.transform.position = new Vector3(-2, 0, 0);
+            //}
+            //else if (random==2)
+            //{
+            //    this.transform.position = new Vector3(2, 0, 0);
+            //}
+            //else if (random==3)
+            //{
+            //    this.transform.position = new Vector3(0, 2, 0);
+            //}
+
+            CheckAvailableNode();
         }
 
         
-        Debug.DrawRay(transform.position + transform.forward * 1f, transform.forward * 3, Color.red);
+        Debug.DrawRay(transform.position + transform.forward, transform.forward * 3, Color.red);
     }
 
+    void CheckAvailableNode()
+    {
+        RaycastHit hit;
+
+        if (Physics.Raycast(transform.GetChild(0).transform.position, transform.GetChild(0).transform.forward, out hit))
+        {
+            Debug.Log("Line hit object name: " + hit.collider.gameObject.name);
+            transform.position = hit.collider.gameObject.transform.position;
+        }
+    }
 
 }
