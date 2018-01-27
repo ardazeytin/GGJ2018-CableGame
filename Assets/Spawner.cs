8d4896292dd9b9dcbb4d08e583d4cd2dd0481026
Arda Zeytin <ardazeytin@outlook.com>
2018-01-27 11:44:01 +0300

    working endless

--- Assets/Spawner.cs
+++ Assets/Spawner.cs
@@ -14,11 +14,6 @@ public class Spawner : MonoBehaviour {
     public GameObject singleCable1;
     public GameObject hitBox;
 
-
-    private Vector3 cableClone1LastPosition;
-
-    private int nodeCounter = 1;
-
     void Start ()
     {
         //centerNodePoint = new Vector3(0, 0, 0);
@@ -26,7 +21,6 @@ public class Spawner : MonoBehaviour {
         //nodePoint2 = new Vector3(1, 1, 0); //top right
         //nodePoint3 = new Vector3(0, -1, 0); //bottom center
 
-        cableClone1LastPosition = nodePoint1.transform.position;
     }
 	
 	// Update is called once per frame
@@ -46,12 +40,10 @@ public class Spawner : MonoBehaviour {
     void SpawnCable()
     {
         //vector3 position will change
-       GameObject cable1Clone = Instantiate(singleCable1, new Vector3(cableClone1LastPosition.x, cableClone1LastPosition.y, cableClone1LastPosition.z + 8), nodePoint1.rotation);
-       cableClone1LastPosition = cable1Clone.transform.position;
-       GameObject cable2Clone = Instantiate(singleCable1, new Vector3(nodePoint2.position.x, nodePoint2.position.y, nodePoint2.transform.position.z + nodeCounter * 8), nodePoint2.rotation);
-       GameObject cable3Clone = Instantiate(singleCable1, new Vector3(nodePoint3.position.x, nodePoint3.position.y, nodePoint3.transform.position.z + nodeCounter * 8), nodePoint3.rotation);
 
        GameObject hitboxClone = Instantiate(hitBox, new Vector3(0, 0, hitBox.transform.position.z + 8), hitBox.transform.rotation);
-       nodeCounter++;
+       GameObject cable1Clone = Instantiate(singleCable1, new Vector3(nodePoint1.position.x, nodePoint1.position.y, hitboxClone.transform.position.z ), nodePoint1.rotation);
+       GameObject cable2Clone = Instantiate(singleCable1, new Vector3(nodePoint2.position.x, nodePoint2.position.y, hitboxClone.transform.position.z ), nodePoint2.rotation);
+       GameObject cable3Clone = Instantiate(singleCable1, new Vector3(nodePoint3.position.x, nodePoint3.position.y, hitboxClone.transform.position.z ), nodePoint3.rotation);
     }
 }
