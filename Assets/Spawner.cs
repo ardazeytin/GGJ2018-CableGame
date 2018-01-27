29925524e38380763b4cf5b39c9e85ec48b54e63
Arda Zeytin <ardazeytin@outlook.com>
2018-01-27 13:36:51 +0300

    new cables

--- Assets/Spawner.cs
+++ Assets/Spawner.cs
@@ -12,6 +12,10 @@ public class Spawner : MonoBehaviour {
 
     //cable prefabs
     public GameObject singleCable1;
+    public GameObject singleCable2;
+    public GameObject singleCable3;
+    public GameObject singleCable4;
+    public GameObject singleCable5;
     public GameObject hitBox;
 
     void Start ()
@@ -40,10 +44,11 @@ public class Spawner : MonoBehaviour {
     void SpawnCable()
     {
         //vector3 position will change
+        int random = Random.Range(1, 6);
 
-       GameObject hitboxClone = Instantiate(hitBox, new Vector3(0, 0, hitBox.transform.position.z + 8), hitBox.transform.rotation);
-       GameObject cable1Clone = Instantiate(singleCable1, new Vector3(nodePoint1.position.x, nodePoint1.position.y, hitboxClone.transform.position.z ), nodePoint1.rotation);
-       GameObject cable2Clone = Instantiate(singleCable1, new Vector3(nodePoint2.position.x, nodePoint2.position.y, hitboxClone.transform.position.z ), nodePoint2.rotation);
-       GameObject cable3Clone = Instantiate(singleCable1, new Vector3(nodePoint3.position.x, nodePoint3.position.y, hitboxClone.transform.position.z ), nodePoint3.rotation);
+       GameObject hitboxClone = Instantiate(hitBox, new Vector3(0, 0, hitBox.transform.position.z + 20), hitBox.transform.rotation);
+       GameObject cable1Clone = Instantiate(singleCable1, new Vector3(nodePoint1.position.x, nodePoint1.position.y, hitboxClone.transform.position.z ), Quaternion.Euler(0,0,0));
+       GameObject cable2Clone = Instantiate(singleCable2, new Vector3(nodePoint2.position.x, nodePoint2.position.y, hitboxClone.transform.position.z ), Quaternion.Euler(0, 0, 0));
+       GameObject cable3Clone = Instantiate(singleCable3, new Vector3(nodePoint3.position.x, nodePoint3.position.y, hitboxClone.transform.position.z ), Quaternion.Euler(0, 0, 0));
     }
 }
